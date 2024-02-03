6019ea42e060a4cb87fc14ec5ae6a4dc236bc17c
alireza <Alirezajj118@gmail.com>
2024-02-04 02:46:07 +0300

    first commit

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,6 @@
+export const Sidebar = () => {
+    return (
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm "
+        >Sidebar!</div>
+    )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+    return ( <div>test page</div> );
+}
+ 
+export default TestPage;
