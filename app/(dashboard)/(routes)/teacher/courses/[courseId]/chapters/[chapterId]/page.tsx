a411afb00b5ab53d207aac352482ecc538207b18
alireza <Alirezajj118@gmail.com>
2024-02-09 23:42:06 +0300

    chapter description added with add , update functionality and preview initialized

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,6 +5,8 @@ import { db } from "@/lib/db";
 import Link from "next/link";
 import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
+import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
     params
@@ -72,7 +74,16 @@ const ChapterIdPage = async ({
                                 Customize your chapter
                             </h2>
                         </div>
-                        {/* ChapterTitleForm */}
+                        <ChapterTitleForm 
+                        initialData={chapter}
+                        courseId={params.courseId}
+                        chapterId={params.chapterId}
+                        />
+                        <ChapterDescriptionForm 
+                        initialData={chapter}
+                        courseId={params.courseId}
+                        chapterId={params.chapterId}
+                        />
                     </div>
                 </div>
             </div>
