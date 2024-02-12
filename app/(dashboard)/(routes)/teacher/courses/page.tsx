1694b2aebac173d9c606bf078cb5ce85da62cfbc
alireza <Alirezajj118@gmail.com>
2024-02-12 03:23:58 +0300

    content table added with fetch function from DB

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,35 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
 
-const CoursesPage = () => {
-    return ( <div>
-        <Link href="/teacher/create">
-        <Button className="p-6">
-            New Course
-        </Button>
-        </Link>
-    </div> );
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+
+
+const CoursesPage = async () => {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+    
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy:{
+            createdAt: 'desc',
+        }
+    });
+
+
+    return ( 
+    <div className="p-6">
+        <DataTable columns={columns} data={courses} />
+    </div> 
+    );
 }
  
 export default CoursesPage;
